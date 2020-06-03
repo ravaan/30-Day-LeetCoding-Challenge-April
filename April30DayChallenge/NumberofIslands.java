aca62aa8f62877107683ee484a85c125c3da4027
ravaan <arpit.agarwal181@gmail.com>
2020-06-03 12:44:29 +0530

    improve code performance and quality

--- April30DayChallenge/NumberofIslands.java
+++ April30DayChallenge/NumberofIslands.java
@@ -1,53 +1,33 @@
 class Solution {
-    public boolean inside(int h, int w, int x, int y){
-        if(x >= 0 && x < h && y >=0 && y < w) return true;
-        return false;
-    }
-    public int numIslands(char[][] grid) {
-        if (grid.length == 0 || grid[0].length == 0) return 0;
-        int h = grid.length;
-        int w = grid[0].length;
-        boolean[][] vis = new boolean[h][w];
-        ArrayList<cord> move = new ArrayList<>(){{
-            add(new cord(1, 0));
-            add(new cord(-1, 0));
-            add(new cord(0, 1));
-            add(new cord(0, -1));
-        }};
-        int answer = 0;
-        for(int i=0; i<h; i++){
-            for( int j=0; j< w; j++){
-                if(!vis[i][j] && grid[i][j] == '1'){
-                    answer++;
-                    Queue<cord> q = new LinkedList<>();
-                    q.add(new cord(i, j));
-                    vis[i][j] = true;
-                    while(q.size() != 0){
-                        cord temp = q.poll();
-                        int x = temp.x;
-                        int y = temp.y;
-                        for(cord cor: move){
-                            int nx = x + cor.x;
-                            int ny = y + cor.y;
-                            if(inside(h, w, nx, ny) && !vis[nx][ny] && grid[nx][ny] == '1'){
-                                q.add(new cord(nx, ny));
-                                vis[nx][ny] = true;
-                            }
-                        }
-                    }
+    int row, col;
+    char[][] grid;
 
+    public int numIslands(char[][] grid) {
+        row = grid.length;
+        if (row == 0) return 0;
+        col = grid[0].length;
+        this.grid = grid;
+
+        int ans = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    ans++;
+                    dfs(i, j);
                 }
             }
         }
-        return answer;
+        return ans;
     }
-}
 
-class cord{
-    int x;
-    int y;
-    public cord(int x, int y){
-        this.x = x;
-        this.y = y;
+    public void dfs(int i, int j) {
+        if (grid[i][j] == '0') return;
+
+        grid[i][j] = '0';
+
+        if (i < row - 1) dfs(i + 1, j);
+        if (i > 0) dfs(i - 1, j);
+        if (j < col - 1) dfs(i, j + 1);
+        if (j > 0) dfs(i, j - 1);
     }
 }
